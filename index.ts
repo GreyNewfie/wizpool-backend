96da8547850a28595b60adb54bb638fc2c91680b
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-10-10 18:48:26 -0230

    Create route for mlbData
    Create a get response to return current MLB data.

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import apiNbaDataRouter from './routers/nbaData';
 import apiNflDataRouter from './routers/nflData';
+import apiMlbDataRouter from './routers/mlbData';
 
 const app = express();
 require('dotenv').config();
 
 app.use('/api/nba_data', apiNbaDataRouter);
 app.use('/api/nfl_data', apiNflDataRouter);
+app.use('/api/mlb_data', apiMlbDataRouter);
 
 app.get('/api', (req, res) => {
 	res.send("You've reached the Wizpool backend!");
