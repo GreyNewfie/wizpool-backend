341a9e0aeff7b75426d2c2ab826228d443292cd3
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-10-10 15:05:30 -0230

    Update dataProcessor for NFL
    Add data processing for NFL data return from the external api.

--- services/dataProcessor.ts
+++ services/dataProcessor.ts
@@ -1,19 +1,40 @@
-import fetchExternalData from './apiService';
-import { TeamData } from '../types/team';
+import fetchExternalData from './externalApiService';
+import { NBATeamData, NFLTeamData } from '../types/team';
 import { ProcessedTeamData } from '../types/processedTeamData';
 
 export default async function processData(
 	league: string,
 	apiUrl: string
 ): Promise<ProcessedTeamData[]> {
-	const rawData: TeamData[] = await fetchExternalData(apiUrl);
+	const rawData: NBATeamData[] | NFLTeamData[] =
+		await fetchExternalData(apiUrl);
 	let processedData: ProcessedTeamData[] = [];
+
 	if (league === 'nba') {
-		processedData = rawData.map((team) => {
+		const nbaData = rawData as NBATeamData[];
+		processedData = nbaData.map((team) => {
 			return {
 				key: team.Key,
 				city: team.City,
 				name: team.Name,
+				conference: team.Conference,
+				division: team.Division,
+				wins: team.Wins,
+				losses: team.Losses,
+			};
+		});
+	}
+
+	if (league === 'nfl') {
+		const nflData = rawData as NFLTeamData[];
+		processedData = nflData.map((team) => {
+			const lastSpaceIndex = team.Name.lastIndexOf(' ');
+			const city = team.Name.substring(0, lastSpaceIndex);
+			const name = team.Name.substring(lastSpaceIndex + 1);
+			return {
+				key: team.Team,
+				city: city,
+				name: name,
 				conference: team.Conference,
 				division: team.Division,
 				wins: team.Wins,
