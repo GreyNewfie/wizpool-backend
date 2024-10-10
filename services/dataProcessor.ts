a7b1987bb1a202a6020de9a668247912f7406161
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-10-10 18:49:12 -0230

    Update dataProcessor.ts
    Add handling processing MLB data.

--- services/dataProcessor.ts
+++ services/dataProcessor.ts
@@ -11,36 +11,59 @@ export default async function processData(
 	let processedData: ProcessedTeamData[] = [];
 
 	if (league === 'nba') {
-		const nbaData = rawData as NBATeamData[];
-		processedData = nbaData.map((team) => {
-			return {
-				key: team.Key,
-				city: team.City,
-				name: team.Name,
-				conference: team.Conference,
-				division: team.Division,
-				wins: team.Wins,
-				losses: team.Losses,
-			};
-		});
 	}
 
 	if (league === 'nfl') {
-		const nflData = rawData as NFLTeamData[];
-		processedData = nflData.map((team) => {
-			const lastSpaceIndex = team.Name.lastIndexOf(' ');
-			const city = team.Name.substring(0, lastSpaceIndex);
-			const name = team.Name.substring(lastSpaceIndex + 1);
-			return {
-				key: team.Team,
-				city: city,
-				name: name,
-				conference: team.Conference,
-				division: team.Division,
-				wins: team.Wins,
-				losses: team.Losses,
-			};
-		});
+	}
+
+	switch (league) {
+		case 'nba':
+			const nbaData = rawData as NBATeamData[];
+			processedData = nbaData.map((team) => {
+				return {
+					key: team.Key,
+					city: team.City,
+					name: team.Name,
+					conference: team.Conference,
+					division: team.Division,
+					wins: team.Wins,
+					losses: team.Losses,
+				};
+			});
+			break;
+		case 'nfl':
+			const nflData = rawData as NFLTeamData[];
+			processedData = nflData.map((team) => {
+				const lastSpaceIndex = team.Name.lastIndexOf(' ');
+				const city = team.Name.substring(0, lastSpaceIndex);
+				const name = team.Name.substring(lastSpaceIndex + 1);
+				return {
+					key: team.Team,
+					city: city,
+					name: name,
+					conference: team.Conference,
+					division: team.Division,
+					wins: team.Wins,
+					losses: team.Losses,
+				};
+			});
+			break;
+		case 'mlb':
+			const mlbData = rawData as MLBTeamData[];
+			processedData = mlbData.map((team) => {
+				return {
+					key: team.Key,
+					city: team.City,
+					name: team.Name,
+					conference: team.League,
+					division: team.Division,
+					wins: team.Wins,
+					losses: team.Losses,
+				};
+			});
+			break;
+		default:
+			console.warn('Unable to process data for the specified league: ', league);
 	}
 
 	return processedData;
