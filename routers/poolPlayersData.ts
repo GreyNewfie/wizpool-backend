8491ad05cdc4c004e7b1644840a78d432fc287fd
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-10-26 15:02:13 -0230

    Add GET route
    Add GET route to retrieve pool players from the database

--- routers/poolPlayersData.ts
+++ routers/poolPlayersData.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { turso } from '../db';
 import { PoolPlayersData } from '../types/pool';
+import { log } from 'console';
 
 const router = Router();
 
@@ -25,11 +26,9 @@ router.post('/', async (req, res) => {
 		console.log(
 			`Pool ${pool_name} player ${player_team_name} has been added to pool_players table`
 		);
-		res
-			.status(201)
-			.json({
-				message: `Pool ${pool_name} player ${player_team_name} added successfully`,
-			});
+		res.status(201).json({
+			message: `Pool ${pool_name} player ${player_team_name} added successfully`,
+		});
 	} catch (error) {
 		console.log('Error adding pool and player to database: ', error);
 		console.log('Pool and player data that failed to add: ', req.body);
@@ -37,6 +36,33 @@ router.post('/', async (req, res) => {
 		res
 			.status(500)
 			.json({ error: 'Failed to add pool and player to database' });
+	}
+});
+
+router.get('/:poolId', async (req, res) => {
+	try {
+		const { poolId } = req.params;
+
+		if (!poolId)
+			return res
+				.status(400)
+				.send('A Pool ID is required in order to get pool players');
+
+		const result = await turso.execute({
+			sql: 'SELECT * FROM pool_players WHERE pool_id = ?',
+			args: [poolId],
+		});
+
+		if (result.rows.length === 0)
+			return res
+				.status(404)
+				.json({ error: `Players for pool ${poolId} not found` });
+
+		const poolPlayers = result.rows;
+		res.status(200).json(poolPlayers);
+	} catch (error) {
+		console.log('Error retrieving pool players from database: ', error);
+		res.status(500).json({ message: 'Failed to retrieve pool players' });
 	}
 });
 
