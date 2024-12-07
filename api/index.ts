ad0576c8ce975f4380427b5f156b79a07d9dcbf9
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-12-07 14:46:00 -0330

    Add get league data route back
    Uncomment league data routes that were thought to be unnecessary because they're required in the frontend.

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
-// import apiNbaDataRouter from '../routers/nbaData';
-// import apiNflDataRouter from '../routers/nflData';
-// import apiMlbDataRouter from '../routers/mlbData';
+import apiNbaDataRouter from '../routers/nbaData';
+import apiNflDataRouter from '../routers/nflData';
+import apiMlbDataRouter from '../routers/mlbData';
 import apiPoolDataRouter from '../routers/poolData';
 // import apiPlayerDataRouter from '../routers/playerData';
 // import apiPoolPlayersDataRouter from '../routers/poolPlayersData';
@@ -25,9 +25,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// app.use('/api/nba_data', apiNbaDataRouter);
-// app.use('/api/nfl_data', apiNflDataRouter);
-// app.use('/api/mlb_data', apiMlbDataRouter);
+app.use('/api/nba_data', apiNbaDataRouter);
+app.use('/api/nfl_data', apiNflDataRouter);
+app.use('/api/mlb_data', apiMlbDataRouter);
 app.use('/api/pools', apiPoolDataRouter);
 // app.use('/api/players', apiPlayerDataRouter);
 // app.use('/api/pool_players', apiPoolPlayersDataRouter);
